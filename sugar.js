0f34ebed7aa3bfddf84c6066e408bb2003d33dd7
lstor <lars@lstor.me>
2016-02-01 23:33:43 +0100

    Update sugar.js

--- sugar.js
+++ sugar.js
@@ -98,3 +98,14 @@ moment.preciseDiff = function(d1, d2) {
 
   return result.join(STRINGS.delimiter);
 };
+
+var daysBetween = function (from, to) {
+  var one_day_in_ms = 1000 * 3600 * 24;
+  return Math.ceil((to.getTime() - from.getTime())/one_day_in_ms) ;
+}
+
+var daysSince = function(day, month, year) {
+  var today = new Date();
+  // subtract 1 to allow 1-based months (jan=1, feb=2, etc.)
+  return daysBetween(new Date(year, month - 1, day), today);
+}
